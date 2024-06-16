9d3d95971bb55918d337e4298d2df2c76d120c0b
PhongLi <longphitran1109@gmail.com>
2024-06-16 21:27:44 +0700

    feat: setup Oauth Github and Google

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,8 @@
 import type { NextAuthConfig } from "next-auth";
 import credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 import bcrypt from "bcryptjs";
 
 import * as schemas from "./schemas";
@@ -7,6 +10,14 @@ import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
     credentials({
       async authorize(credentials) {
         const validatedFields = schemas.LoginSchema.safeParse(credentials);
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -4,13 +4,21 @@ import { FaGithub } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Button } from "@/components/ui/button";
 
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
+
 export const Social = () => {
+  const onClick = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
     <div className='w-full flex items-center gap-x-2'>
-      <Button variant='outline' size='lg' className='w-full' onClick={() => {}}>
+      <Button variant='outline' size='lg' className='w-full' onClick={() => onClick("google")}>
         <FcGoogle className='w-5 h-5' />
       </Button>
-      <Button variant='outline' size='lg' className='w-full' onClick={() => {}}>
+      <Button variant='outline' size='lg' className='w-full' onClick={() => onClick("github")}>
         <FaGithub className='w-5 h-5' />
       </Button>
     </div>
